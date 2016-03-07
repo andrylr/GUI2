5a6e874227068245137dbea97ef10580ffa9c9a3
Andry <andrylr@gmail.com>
2016-03-07 11:01:35 -0500

    Template error needs fix

---
+++ ClockWork/client/login/login.component.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+if (Meteor.isClient) {
+    Template.register.events({
+        'submit form': function(event){
+            event.preventDefault();
+            var email = event.target.registerEmail.value;
+            var password = event.target.registerPassword.value;
+            Accounts.createUser({
+                email: email,
+                password: password
+            });
+        }
+    });
+
+    Template.login.events({
+        'submit form': function(event) {
+            event.preventDefault();
+            var email = event.target.loginEmail.value;
+            var password = event.target.loginPassword.value;
+            Meteor.loginWithPassword(email, password);
+        }
+    });
+}
+
+
+
+
